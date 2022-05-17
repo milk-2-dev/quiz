edad5ed568bfa89d81e7a20684ab17eb06e32739
Roman <papachiv.roman@gmail.com>
2022-05-17 22:18:08 +0300

    fix: add sass modules

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import classes from './Layout.module.css'
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
 import Drawer from '../../components/Navigation/Drawer/Drawer'
 import { useSelector } from 'react-redux'
+import classes from './Layout.module.scss'
 
 function Layout({ children }) {
   const [menu, setMenu] = useState(false)
@@ -16,14 +16,48 @@ function Layout({ children }) {
     setMenu(false)
   }
 
+  const renderMenu = () => {
+    return (
+      <React.Fragment>
+        <Drawer
+          isOpen={menu}
+          onClose={onCloseHandler}
+          isAuthenticated={auth.isAuthenticated}
+        />
+        <MenuToggle onToggle={toggleMenuHandler} isOpen={menu} />
+      </React.Fragment>
+    )
+  }
+
   return (
     <div className={classes.Layout}>
-      <Drawer
-        isOpen={menu}
-        onClose={onCloseHandler}
-        isAuthenticated={auth.isAuthenticated}
-      />
-      <MenuToggle onToggle={toggleMenuHandler} isOpen={menu} />
+      <div
+        className={`${classes.circle} ${classes.circle_xl} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.circle} ${classes.circle_sm} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.triangle} ${classes.triangle_sm} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.circle} ${classes.circle_sm} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.circle} ${classes.circle_xl} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.ring} ${classes.ring_sm} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.ring} ${classes.ring_sm} ${classes.random_position}`}
+      ></div>
+      <div
+        className={`${classes.triangle} ${classes.triangle_sm} ${classes.random_position}`}
+      ></div>
+
+      {auth.isAuthenticated ? renderMenu() : null}
+
       <main>{children}</main>
     </div>
   )
