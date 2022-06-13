1550f377beaebe5f6d3f4734c58e6641fa57b815
Roman <papachiv.roman@gmail.com>
2022-06-13 17:52:10 +0300

    hack: handle admin user

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,26 +5,34 @@ import {
 } from 'firebase/auth'
 import { authFirebaseSDK } from '../firebase'
 
-const setLocalStorageData = (localId: string) => {
+const setLocalStorageData = (localId: string, email: string) => {
+  localStorage.setItem('email', email)
   localStorage.setItem('userId', localId)
 }
 
 const clearLocalStorageData = () => {
   localStorage.removeItem('userId')
+  localStorage.removeItem('email')
 }
 
 type authFunc = (email: string, password: string) => {}
 
-const signUp: authFunc = (email, password) => {
-  return createUserWithEmailAndPassword(authFirebaseSDK, email, password).then(
-    (response) => setLocalStorageData(response.user.uid)
+const signUp: authFunc = async (email, password) => {
+  const response = await createUserWithEmailAndPassword(
+    authFirebaseSDK,
+    email,
+    password
   )
+  return setLocalStorageData(response.user.uid, response.user.email)
 }
 
-const signIn: authFunc = (email, password) => {
-  return signInWithEmailAndPassword(authFirebaseSDK, email, password).then(
-    (response) => setLocalStorageData(response.user.uid)
+const signIn: authFunc = async (email, password) => {
+  const response = await signInWithEmailAndPassword(
+    authFirebaseSDK,
+    email,
+    password
   )
+  return setLocalStorageData(response.user.uid, response.user.email)
 }
 
 const singOut = () => {
